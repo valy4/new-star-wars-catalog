975e7bc4e0fd6c5d23c2164d0e5ca4589782c45d
Bogdan Nedelcu <bogdanned32@gmail.com>
2021-07-22 08:47:55 +0200

    feedback

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -1,48 +1,42 @@
-import React from "react"
-import { useState, useEffect } from "react"
-import VehicleCard from "./Vehicles/VehiclesCard"
-import styled from "styled-components"
-import { Link } from "react-router-dom"
-
+import React from "react";
+import { useState, useEffect } from "react";
+import VehicleCard from "./Vehicles/VehiclesCard";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 function FirstPage() {
-
-  const [vehiclesDetails, setVehiclesDetails] = useState([])
+  const [vehiclesDetails, setVehiclesDetails] = useState([]);
 
   useEffect(() => {
     fetch(`https://swapi.dev/api/vehicles/`)
-      .then(response => response.json())
+      .then((response) => response.json())
       .then((data) => {
-        console.log(data)
-        setVehiclesDetails(data.results)
-
-
-      })
-  }, [])
+        console.log(data);
+        setVehiclesDetails(data.results);
+      });
+  }, []);
   return (
-    <Link to={`/vehicle/${vehiclesDetails.url && vehiclesDetails.url.split("/")[5]}`}>
-      <ContainerPage>
-        {vehiclesDetails.map((vehicle) => (<VehicleCard
-          model={vehicle.model}
-          manufacturer={vehicle.manufacturer}
-          vehicle_class={vehicle.vehicle_class}
-          passengers={vehicle.passengers}
-          crew={vehicle.crew}
-          name={vehicle.name}
-          id={vehicle.url.split("/")[5]}
-        >
-        </VehicleCard>)
-
-        )}
-
-      </ContainerPage>
-    </Link >
-  )
+    <ContainerPage>
+      {vehiclesDetails.map((vehicle, index) => (
+        <Link to={`/vehicle/${vehicle.url && vehicle.url.split("/")[5]}`} id={index}>
+          <VehicleCard
+            model={vehicle.model}
+            manufacturer={vehicle.manufacturer}
+            vehicle_class={vehicle.vehicle_class}
+            passengers={vehicle.passengers}
+            crew={vehicle.crew}
+            name={vehicle.name}
+            id={vehicle.url.split("/")[5]}
+          ></VehicleCard>{" "}
+        </Link>
+      ))}
+    </ContainerPage>
+  );
 }
 
-export default FirstPage
+export default FirstPage;
 const ContainerPage = styled.div`
-      display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
-      `
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
